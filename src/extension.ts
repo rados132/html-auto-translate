2fa794606b378bc042fced7ca1db53eb4a0ef030
rados132 <radosradovanovic@gmail.com>
2022-07-20 22:23:03 +0200

    Test

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('html-auto-translate.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('html-auto-translate.test', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from html-auto-translate!');
+		vscode.window.showInformationMessage('Ovo ce biti super ekstenzija!');
 	});
 
 	context.subscriptions.push(disposable);
